ef8f31ace2c1bf5159d538e38b421820a40befd0
OleksandrPro <oleksandr.proshkin@gmail.com>
2023-06-28 17:05:57 +0300

    Cram 1.0

---
+++ MainForm.cpp
@@ -0,0 +1,11 @@
+#include "MainForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Lab4::MainForm form;
+	Application::Run(% form);
+}
